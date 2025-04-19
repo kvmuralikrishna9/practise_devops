340be134b01fb8424edc59d63f53033076b16984
kvmuralikrishn9 <kvmuralikrishn9@outlook.com>
2025-04-19 13:59:29 +0530

    added array example

--- roboshop/shell_script/scripting_training/6_array.sh
+++ roboshop/shell_script/scripting_training/6_array.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# This is example file for using array in shell script
 
-user_name=("Murali" "Himanish" "Swarna")
+user=("murali" "swarna" "himanish")
 
-echo -e "hello all, we have the ${user_name[1]} with us.\n"
-echo -e "we also present you the ${user_name[2]} to teach devops\n"
-echo "we all ${user_name[@]} to present the meeting"
+echo "Hello all, we welcome ${user[@]}, to the party"
