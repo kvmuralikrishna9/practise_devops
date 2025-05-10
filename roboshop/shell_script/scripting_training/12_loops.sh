244edae02a14f2990f3292e3270cbf217217db93
kvmuralikrishn9 <kvmuralikrishn9@outlook.com>
2025-05-10 16:46:13 +0530

    modified 8_install.sh

--- roboshop/shell_script/scripting_training/12_loops.sh
+++ roboshop/shell_script/scripting_training/12_loops.sh
@@ -11,19 +11,19 @@ USER_ID=$(id -u)
 PACKAGES=("mysql" "postfix" "git")
 
 if [[ $USER_ID -ne 0 ]] ; then
-    echo -e "\nuser is not the 'root', use root user to continue. . ."
+    echo -e "\nuser is not the 'root', use root user to continue. . ." &2>>LOG_FILE
     exit 1
 else
-    echo -e "\nuser is 'root', continuing to install the packages. . ."
+    echo -e "\nuser is 'root', continuing to install the packages. . ." &2>>LOG_FILE
 fi
 
 for i in $@ ; do
-    yum list installed $i
+    yum list installed $i &2>>LOG_FILE
     if [[ $? -ne 0 ]] ; then
-        echo -e "$i is not installed,,"
-        yum install $i -y
+        echo -e "$i is not installed,," &2>>LOG_FILE
+        yum install $i -y &2>>LOG_FILE
     else
-        echo -e "$i is already installed,,"  
+        echo -e "$i is already installed,," &2>>LOG_FILE 
     fi
 done
 
