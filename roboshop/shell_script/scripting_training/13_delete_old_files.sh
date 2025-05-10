0d5152cc5bf0a8d87526ae469788a930b49b1638
kvmuralikrishn9 <kvmuralikrishn9@outlook.com>
2025-05-10 20:56:18 +0530

    adding deleting old files

--- roboshop/shell_script/scripting_training/13_delete_old_files.sh
+++ roboshop/shell_script/scripting_training/13_delete_old_files.sh
@@ -5,9 +5,9 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 OUTPUT_LOG=/tmp/$SCRIPT_NAME-$DATE.log
 
-FILES_TO_DELETE=$(find $DELETE_LOGS_FROM -name "*.log" -type f -mtime +14)
+FILES_TO_DELETE=$(find $DELETE_LOGS_FROM -name "*.log" -type f -mtime +14) &>>$OUTPUT_LOG
 
-while read line ; do
-    echo  "Deleting the old files"
+while read line ; do 
+    echo  "Deleting the old files" &>>$OUTPUT_LOG
     rm -rf $line
 done <<< $FILES_TO_DELETE
